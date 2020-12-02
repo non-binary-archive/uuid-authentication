5282566480fc702ee1d1d3566ee8f6f976026f49
Luna Graysen <luna.graysen@bitrise.io>
2020-12-02 13:17:10 -0500

    Backend Hello World application

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+// Initialize the application.
+#[tokio::main]
+async fn main() {
+    // Initialize Rocket, mount root route and register 404 catcher.
+    match rocket::ignite().mount("/", routes![response]).register(catchers![not_found]).launch().await {
+        // If success, print success.
+        Ok(success) => println!("success: {:?}", success),
+        // If error, print error.
+        Err(error) => println!("error: {:?}", error)
+    }
+}
+
+// Routes
+#[get("/")]
+pub async fn response() -> String {
+    // Temp: Return Hello World string.
+    "Hello World".to_string()
+}
+
+#[catch(404)]
+pub fn not_found() -> &'static str {
+    // Catch all in case someone goes to the URL directly.
+    "You are using this tool incorrectly. Please use it through the API."
 }
